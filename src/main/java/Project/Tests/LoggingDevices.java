20541126ccc976f0e25cf0d4d01bb0ed7475db84
liran.hochman <liran345@gmail.com>
2019-04-17 11:50:14 +0300

    test - logging device

---
+++ src/main/java/Project/Tests/LoggingDevices.java
@@ -0,0 +1,33 @@
+package Project.Tests;
+
+import Project.BaseTest;
+import Project.Main;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoggingDevices extends BaseTest {
+
+
+    @DisplayName("LoggingDevices_test")
+    @Test
+    public void LoggingDevices_test() {
+        Main.sout("Info!", "Starting testLoggingDevices_test for device " + device.getSerialnumber()+".txt");
+
+            client.startLoggingDevice(Main.innerDirectoryPath+"\\"+device.getSerialnumber()+"\\"+testStartTime);
+            client.sleep(1000*30);
+
+        try {
+            client.stopLoggingDevice();
+        } catch (Exception e) {
+            client.report(e.getMessage(),false);
+            Main.sout("Exception | LoggingDevices ",device.getSerialnumber(),e.getMessage());
+            driver.quit();
+            throw e;
+        }
+    }
+
+}
