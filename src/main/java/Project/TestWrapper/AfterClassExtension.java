2fd60e0c7f54131b495b721c272c5d8823df29bf
liran.hochman <liran345@gmail.com>
2019-06-23 15:41:44 +0300

    Fix the after class extension reporter

--- src/main/java/Project/TestWrapper/AfterClassExtension.java
+++ src/main/java/Project/TestWrapper/AfterClassExtension.java
@@ -9,149 +9,175 @@ import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.util.Optional;
+
 public class AfterClassExtension implements AfterEachCallback {
     boolean deviceOS_isIOS;
-    Agent agent;
+    boolean isEspresso_XCUI= false;
+    Agent agent = new Agent("","");
+    String sessionID="";
+    String testName ="";
+    String deviceSN="";
+    String reportPath="";
+    String EndTime ="";
+    String StartTime = "";
+    long testDuring = 0;
 
     @Override
     public void afterEach(ExtensionContext context) throws Exception {
 
-        Main.sout("Info","afterTestExecution for device: "+ Thread.currentThread().getName());
-        BaseTest baseTest = ((BaseTest)context.getTestInstance().get());
+       Main.sout("Info", "afterTestExecution for device: " + Thread.currentThread().getName());
+        BaseTest baseTest = ((BaseTest) context.getTestInstance().get());
         Boolean testResult = !context.getExecutionException().isPresent(); //true - SUCCESS, false- FAILED
 
-        Optional<Throwable> executionException=context.getExecutionException();
-        String sessionID = baseTest.sessionID;
-        String testName = context.getDisplayName();
+        Optional<Throwable> executionException = context.getExecutionException();
 
-        String deviceSN= baseTest.device.getSerialnumber();
-        agent = baseTest.device.getAgent();
-        deviceOS_isIOS= baseTest.device.isIOS();
+
         String ReporterStatus = "null";
         try {
             ReporterStatus = Api_Reporter.GetTestResultStatus(baseTest.testID);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("ReporterStatus is null");
 //            ReporterStatus += " - "+ e.getMessage();
         }
 
-        String reportPath = baseTest.reportURL;
-        if(!reportPath.contains("http") && !reportPath.contains("ReportURL")) {
-            reportPath = reportPath + "/index.html";
-        }
-        String EndTime=String.valueOf(baseTest.testEndTime);
-        String StartTime=String.valueOf(baseTest.testStartTime);
-        long testDuring =System.currentTimeMillis() - baseTest.testStartTime_calculate;
+        try {
+            sessionID = baseTest.sessionID;
+            testName = context.getDisplayName();
+
+            deviceSN = baseTest.device.getSerialnumber();
+            agent = baseTest.device.getAgent();
+            deviceOS_isIOS = baseTest.device.isIOS();
+            isEspresso_XCUI = baseTest.isEspresso_XCUI;
 
 
-        Main.countTests++;
-
-
-        boolean incompleteTest = false;
-        if(ReporterStatus.equals("null")||ReporterStatus.equals("Incomplete")){
-            incompleteTest = true;
-        }
-
-        if(testResult.equals(false)){
-            System.err.println("afterTestExecution - FAIL \t"+"+Thread.currentThread().getName() "+Thread.currentThread().getName()+"\t devicesn: "+deviceSN);
-            String error=executionException.toString().replaceAll("\n"," | ");
-            Main.report.addRowToReport(getDeviceOSinString(),testName, deviceSN,agent.getAgentName(),String.valueOf(testResult), ReporterStatus,StartTime ,EndTime,calculateTestDuring(testDuring),sessionID,reportPath,error);
-//           System.out.println("the test failed");
-            Main.countTests_fail++;
-
-            if(incompleteTest){
-                addTestResultToAgent("Incomplete");
-            }else {
-                addTestResultToAgent(false);
+            reportPath = baseTest.reportURL;
+            if (!reportPath.contains("http") && !reportPath.contains("ReportURL")) {
+                reportPath = reportPath + "/index.html";
             }
-            addToSummaryReport(false);
-        }
+            EndTime = String.valueOf(baseTest.testEndTime);
+            StartTime = String.valueOf(baseTest.testStartTime);
+            testDuring = System.currentTimeMillis() - baseTest.testStartTime_calculate;
 
-        if(testResult.equals(true)){
-            System.err.println("afterTestExecution - PASS \t"+"+Thread.currentThread().getName() "+Thread.currentThread().getName()+"\t device sn: "+deviceSN);
-            Main.report.addRowToReport(getDeviceOSinString(),testName, deviceSN,agent.getAgentName(),String.valueOf(testResult),ReporterStatus, StartTime , EndTime,calculateTestDuring(testDuring),sessionID,reportPath,"");
-//            System.out.println("the test passed");
-            Main.countTests_pass++;
 
-            if(incompleteTest){
-                addTestResultToAgent("Incomplete");
+
+            Main.countTests++;
+
+
+            boolean incompleteTest = false;
+            if (ReporterStatus.equals("null") || ReporterStatus.equals("Incomplete")) {
+                incompleteTest = true;
+            }
+
+        }finally {
+
+            boolean incompleteTest = false;
+            if (ReporterStatus.equals("null") || ReporterStatus.equals("Incomplete")) {
+                incompleteTest = true;
+            }
+
+            if (testResult.equals(false)) {
+                System.err.println("afterTestExecution - FAIL \t" + "+Thread.currentThread().getName() " + Thread.currentThread().getName() + "\t devicesn: " + deviceSN);
+                String error = executionException.toString().replaceAll("\n", " | ");
+                Main.report.addRowToReport(getDeviceOSinString(), testName, deviceSN, agent.getAgentName(), String.valueOf(testResult), ReporterStatus, StartTime, EndTime, calculateTestDuring(testDuring), sessionID, reportPath, error);
+//           System.out.println("the test failed");
+                Main.countTests_fail++;
+
+                if (incompleteTest) {
+                    addTestResultToAgent("Incomplete");
+                } else {
+                    addTestResultToAgent(false);
+                }
                 addToSummaryReport(false);
             }
-            else {
-                addTestResultToAgent(true);
-                addToSummaryReport(true);
-            }
 
+            if (testResult.equals(true)) {
+                System.err.println("afterTestExecution - PASS \t" + "+Thread.currentThread().getName() " + Thread.currentThread().getName() + "\t device sn: " + deviceSN);
+                Main.report.addRowToReport(getDeviceOSinString(), testName, deviceSN, agent.getAgentName(), String.valueOf(testResult), ReporterStatus, StartTime, EndTime, calculateTestDuring(testDuring), sessionID, reportPath, "");
+//            System.out.println("the test passed");
+                Main.countTests_pass++;
+
+                if (incompleteTest) {
+                    addTestResultToAgent("Incomplete");
+                    addToSummaryReport(false);
+                } else {
+                    addTestResultToAgent(true);
+                    addToSummaryReport(true);
+                }
+
+            }
         }
     }
 
 
-    public void addTestResultToAgent(boolean result){
+    public void addTestResultToAgent(boolean result) {
         agent.addTestToAgent(result);
     }
 
-    public void addTestResultToAgent(String result){
+    public void addTestResultToAgent(String result) {
         agent.addTestToAgent(result);
     }
 
 
-
-    protected void addToSummaryReport(boolean result){
+    protected void addToSummaryReport(boolean result) {
         try {
             SummaryReport.getAgentsPer();
-        }catch (Exception e){
-            System.out.println("SummaryReport.getAgentsPer failed "+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("SummaryReport.getAgentsPer failed " + e.getMessage());
         }
         SummaryReport.add1_Total_TestsNum();
-        if(!result){
+        if (!result) {
             SummaryReport.add1_Total_TestsFailed();
         }
-        if(deviceOS_isIOS){
+        if (deviceOS_isIOS) {
             SummaryReport.add1_IOS_TestsNum();
-            if(!result){
+            if (!result) {
                 SummaryReport.add1_IOS_TestsFailed();
             }
 
-            }else{ //Android
+        } else { //Android
             SummaryReport.add1_Android_TestsNum();
-            if(!result){
+            if (!result) {
                 SummaryReport.add1_Android_TestsFailed();
             }
         }
 
     }
 
-    protected String getDeviceOSinString(){
-        if(deviceOS_isIOS){
-            return "IOS";
-        }
-        else{
-            return "Android";
-        }
+    protected String getDeviceOSinString() {
+        String DeviceOSinString="";
+        if (deviceOS_isIOS) {
+            DeviceOSinString = "IOS";
+        } else {
+            DeviceOSinString ="Android";
         }
 
+        if(isEspresso_XCUI){
+            DeviceOSinString += " - Espresso_XCUI";
+        }
 
-    protected String calculateTestDuring(long TestDuring){
+        return DeviceOSinString;
+    }
+
+
+
+
+    protected String calculateTestDuring(long TestDuring) {
         System.out.println("Calculating test during");
-        String TestDuringForPrint="";
+        String TestDuringForPrint = "";
         //Move from milliseconds to seconds
-        TestDuring=TestDuring/1000;
-        if(TestDuring<=60){
-            TestDuringForPrint=String.valueOf(TestDuring)+ " Sec";
-        }
-        else
-        if(TestDuring>60 && TestDuring<60*60){
-            long Sec=TestDuring%60;
-            TestDuring=TestDuring/60 ;
-            TestDuringForPrint=String.valueOf(TestDuring)+ " Min "+String.valueOf(Sec)+" Sec";
-        }
-        else
-        if(TestDuring>60*60){
-            long Min=TestDuring%60;
-            TestDuring=TestDuring/60;
-            long Sec=TestDuring%60;
-            TestDuring=TestDuring/60;
-            TestDuringForPrint=String.valueOf(TestDuring)+ " Hour "+String.valueOf(Min)+ " Min "+String.valueOf(Sec)+" Sec";
+        TestDuring = TestDuring / 1000;
+        if (TestDuring <= 60) {
+            TestDuringForPrint = String.valueOf(TestDuring) + " Sec";
+        } else if (TestDuring > 60 && TestDuring < 60 * 60) {
+            long Sec = TestDuring % 60;
+            TestDuring = TestDuring / 60;
+            TestDuringForPrint = String.valueOf(TestDuring) + " Min " + String.valueOf(Sec) + " Sec";
+        } else if (TestDuring > 60 * 60) {
+            long Min = TestDuring % 60;
+            TestDuring = TestDuring / 60;
+            long Sec = TestDuring % 60;
+            TestDuring = TestDuring / 60;
+            TestDuringForPrint = String.valueOf(TestDuring) + " Hour " + String.valueOf(Min) + " Min " + String.valueOf(Sec) + " Sec";
         }
 
 
@@ -160,25 +186,22 @@ public class AfterClassExtension implements AfterEachCallback {
 
     }
 
-    protected String calculateTestDuring_old(long TestDuring){
+    protected String calculateTestDuring_old(long TestDuring) {
         System.out.println("Calculating test during");
-        TestDuring=TestDuring/1000;
-        String Units=" Sec";
-        if (TestDuring>60 && TestDuring<60*60){
-            long second = TestDuring%60;
-            TestDuring=TestDuring/60 ;
-            Units=" Min";
-        }
-        else
-        if (TestDuring>60*60){
-            TestDuring=TestDuring/(60*60);
-            Units=" Hour";
+        TestDuring = TestDuring / 1000;
+        String Units = " Sec";
+        if (TestDuring > 60 && TestDuring < 60 * 60) {
+            long second = TestDuring % 60;
+            TestDuring = TestDuring / 60;
+            Units = " Min";
+        } else if (TestDuring > 60 * 60) {
+            TestDuring = TestDuring / (60 * 60);
+            Units = " Hour";
         }
 
-        System.out.println(String.valueOf(TestDuring)+Units);
-        return String.valueOf(TestDuring)+Units;
+        System.out.println(String.valueOf(TestDuring) + Units);
+        return String.valueOf(TestDuring) + Units;
     }
-
 
 
 }
